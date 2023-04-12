0442ba79d88e915d25477834f28099b45c2ab2cd
iNFiENiTE-World <infienite@gmail.com>
2023-04-12 20:50:34 +0800

    Refactored codes and added error handlers across input handlers

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 # This script will mass download soundtracks of an album from downloads.khinsider.com
-import os.path
+# Built-in dependencies
+from os.path import isfile
 from os import mkdir
 from pathlib import Path
+from typing import Dict, Tuple, List
 from urllib.parse import unquote
 
-import requests
+# External dependencies
+from requests import get
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-def get_input(prompt, default=None):
+class InvalidUrl(Exception):
+    pass
+
+
+def get_input(prompt: str, default: str = None) -> str:
     """
-    Get user input with a default value if none is provided.
-    :param prompt:
-    :param default:
-    :return:
+    Prompts the user for input with the given prompt message and returns the user's response.
+    If the user enters nothing and a default value is provided, returns the default value.
+
+    Args:
+        prompt (str): The message to display to the user when prompting for input.
+        default (str, optional): The default value to return if the user enters nothing. Default is None.
+
+    Returns:
+        str: The user's input, or the default value if provided and the user entered nothing.
     """
     return input(prompt) or default
 
 
-def display_album(title: str, total_duration: str, audios: dict):
+def display_album(title: str, total_duration: str, audios: Dict[str, str]):
     """
-    Display album information in the terminal
-    :param title:
-    :param total_duration:
-    :param audios:
-    :return:
+    Displays information about an album, including its title, total duration,
+    and available audio formats and their corresponding file sizes.
+
+    Args:
+        title (str): The title of the album.
+        total_duration (str): The total duration of the album.
+        audios (dict): A dictionary containing the available audio formats and their corresponding file sizes.
+
+    Returns:
+        None
     """
-    print('\nALBUM')
+    print('ALBUM')
     print(f'Title: {title}')
     print(f'Total Duration: {total_duration}')
     print('Available format:')
@@ -35,26 +52,43 @@ def display_album(title: str, total_duration: str, audios: dict):
         print(f'✓ {format_} ({space})')
 
 
-def mb_to_gb(megabytes):
+def mb_to_gb(megabytes: int | float):
+    """
+    Convert a value in megabytes to gigabytes.
+
+    Args:
+        megabytes (int or float): A value in megabytes to convert to gigabytes.
+
+    Returns:
+        float: The equivalent value in gigabytes, rounded to two decimal places.
+
+    Examples:
+        >>> mb_to_gb(5000)
+        5.0
+        >>> mb_to_gb(12345.67)
+        12.35
+    """
     return round(megabytes / 1000, 2)
 
 
-
-def get_album_info(url):
+def get_album_info(url: str) -> Tuple[str, str, Dict[str, str], List[str]]:
     """
-    Extracts album information from a Khinsider album page URL.
+    Retrieves information about an album from downloads.khinsider.com
 
     Args:
-        url (str): The URL of the Khinsider album page.
+        url: A string representing the URL of the album page.
 
     Returns:
-        tuple: A tuple containing the available audio formats (list of str),
-               the total time of the soundtrack (str), and the space requirements
-               for each audio format (list of str).
+        A tuple containing the album title, total duration, dictionary of available audio formats and their sizes,
+        and a list of URLs to the soundtrack source pages.
     """
 
+    if 'https://downloads.khinsider.com/game-soundtracks/album/' not in url:
+        raise InvalidUrl('Invalid URL: URL must contain https://downloads.khinsider.com/game-soundtracks/album'
+                         '/<album_name>')
+
     # Make a request to the album page URL and parse the HTML with BeautifulSoup
-    html = requests.get(url)
+    html = get(url)
     album_page = BeautifulSoup(html.text, 'html.parser')
 
     # Get the album title.
@@ -76,7 +110,15 @@ def get_album_info(url):
 
     duration, *spaces = ths[-(len(audio_formats) + 2):-1]
     duration = duration.text
-    spaces = list(map(lambda s: f"{mb_to_gb(int((s.text.split(' ')[0]).replace(',', '')))} GB", spaces))
+
+    spaces = list(map(lambda s: int((s.text.split(' ')[0]).replace(',', '')), spaces))
+
+    more_than_1000 = [s for s in spaces if s >= 1000]
+
+    if more_than_1000:
+        spaces = list(map(lambda s: f'{mb_to_gb(s)} GB', spaces))
+    else:
+        spaces = list(map(lambda s: f'{s} MB', spaces))
 
     # Create dictionary containing formats and its corresponding size
     audios = dict(zip(audio_formats, spaces))
@@ -88,70 +130,96 @@ def get_album_info(url):
     return album_title, duration, audios, soundtracks_page_url
 
 
-def choose_audio_format(audios: dict, prompt='Choose a format:'):
+def choose_audio_format(audios: Dict[str, str], prompt: str = 'Choose a format:') -> str:
     """
-    Generate index for audio format and prompt the user to choose an audio format by typing the index.
-    :param audios:
-    :param prompt:
-    :return:
+    Allows the user to choose an audio format from a given dictionary of audio formats and their corresponding sizes.
+
+    Args:
+        audios (Dict[str, str]): A dictionary containing audio formats and their corresponding sizes.
+        prompt (str, optional): The prompt message to display to the user. Defaults to "Choose a format:".
+
+    Returns:
+        str: The chosen audio format.
     """
     print(prompt)
+
     for i, audio in enumerate(audios.items()):
         print(f'{i} - {audio[0]}')
-    return get_input('', default=0)
+
+    length = len(audios)
+    while True:
+        try:
+            input_ = get_input("", default='0')
+            selection = range(0, length)
+            if int(input_) in list(range(0, length)):
+                return input_
+            raise Exception
+        except Exception as err:
+            print(f"Invalid input: Please enter number {' or '.join(map(lambda t: str(t), selection))} only.")
 
 
-def prepare_download_directory(dir_path):
+def prepare_download_directory(default: str):
     """
-    Create new directory if the directory doesn't exist.
-    :param dir_path:
-    :return:
+    Creates a new directory at the given path if it does not already exist.
+
+    Args:
+        default: A string representing the default path to the directory to create.
+    Returns:
+        None
     """
-    try:
-        mkdir(dir_path)
-    except FileExistsError as error:
-        pass
+    while True:
+        out = str(get_input(f'Download location (Press Enter to use default: {default} ):\n', default=default))
+
+        try:
+            mkdir(out)
+            return out
+        except FileExistsError:
+            return out
+        except Exception:
+            print('Invalid location: Please provide a valid download location or use default.\n')
 
 
-def download_soundtracks(soundtracks_page_url, audio_format, dir_out):
-    """
-    Downloads the soundtracks of specified format to the specified output directory.
-    :param soundtracks_page_url:
-    :param audio_format:
-    :param dir_out:
-    :return:
+def download_soundtracks(soundtracks_page_url: List[str], audio_format: int, dir_out: str) -> None:
+    """Download the soundtracks listed in the given URLs in the specified audio format.
+
+    Args:
+        soundtracks_page_url (List[str]): URLs of the pages where the soundtracks are listed.
+        audio_format (int): Index of the audio format to download from the list of available formats for each soundtrack.
+        dir_out (str): Path to the directory where the downloaded files will be saved.
+
+    Returns:
+        None. The function downloads the files and displays progress information on the console.
     """
     print(f'{len(soundtracks_page_url)} files will be downloaded...')
-    downloaded = 0
+    download_count = 0
     for url in soundtracks_page_url:
         # Open source page
-        html = requests.get(url)
-        soundtrack_page = BeautifulSoup(html.text, 'html.parser')
+        soundtrack_page = BeautifulSoup(get(url).text, 'html.parser')
 
         # Scrape the link to download resource
-        source = soundtrack_page.find_all(class_='songDownloadLink')[audio_format].parent['href']
+        source_url = soundtrack_page.find_all(class_='songDownloadLink')[audio_format].parent['href']
 
         # The website's url already formatted some characters to their corresponding code (%XX)
         # Unquote will reformat it from code back to its original character
         # Refer https://docs.python.org/3.10/library/urllib.parse.html#urllib.parse.unquote
-        source = unquote(source)
-        source = source.replace('#', '%23')  # The '#' character is bugged when URL encoded, this fixes it!
+        # The '#' special character is used in certain URLs (in the filename), replace for URL purpose
+        source_url = unquote(source_url).replace('#', '%23')
 
-        filename = source.rsplit('/', 1)[1]
+        filename = source_url.rsplit('/', 1)[1].replace('%23', '#')
         filepath = f'{dir_out}/{filename}'
 
         # Checks if file already exists
-        if os.path.isfile(filepath):
+        if isfile(filepath):
             print(f'Skipping {filename}, it already exists...')
             continue
 
         print(f'Downloading {filename}...')
-        # Code and comment generated by ChatGPT-3
+
         # Make a GET request to the URL, but don't download the entire response at once
-        response = requests.get(source, stream=True)
+        response = get(source_url, stream=True)
 
         total_size_in_bytes = int(response.headers.get('content-length', 0))
-        block_size = 1024  # 1 Kilobyte
+        block_size = 1048576  # 1MB
 
         progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 
@@ -163,26 +231,57 @@ def download_soundtracks(soundtracks_page_url, audio_format, dir_out):
 
         progress_bar.close()
 
-        downloaded += 1
-    print(f'Downloads complete! {downloaded} files have been downloaded')
+        download_count += 1
+    print(f'Downloads finished! {download_count} files have been downloaded')
 
 
-# URL to the album's page
-album_url = str(get_input("Link to the album's page (from downloads.khinsider.com):\n"))
+while True:
+    # URL to the album's page
+    album_url = str(get_input("Link to the album's page (from downloads.khinsider.com):\n"))
 
-# Get soundtrack information
-album_title, duration, audios, soundtracks_page_url = get_album_info(album_url)
+    try:
+        # Get soundtrack information
+        album_title, duration, audios, soundtracks_page_url = get_album_info(album_url)
 
-# Display the information
-display_album(album_title, duration, audios)
+        print('')
 
-# Choose an audio format if there are more than one format available
-audio_format = int(choose_audio_format(audios, '\nChoose a format:') if len(audios) > 1 else 0)
+        # Display the information
+        display_album(album_title, duration, audios)
 
-# Preset the output directory of audio file
-dir_out = f'{str(Path.home())}/Music/{album_title}'
-dir_out = str(get_input(f'\nDownload location (Press Enter to use default: {dir_out} ):\n', default=dir_out))
-prepare_download_directory(dir_out)
+        print('')
 
-print('\nPreparing download...')
-download_soundtracks(soundtracks_page_url, audio_format, dir_out)
+        # Choose an audio format if there are more than one format available
+        audio_format = int(choose_audio_format(audios, '\nChoose a format:') if len(audios) > 1 else 0)
+
+        print('')
+
+        # Preset the output directory of audio file
+        dir_out = prepare_download_directory(f'{str(Path.home())}/Music/{album_title}')
+
+        print('\nPreparing download...')
+        download_soundtracks(soundtracks_page_url, audio_format, dir_out)
+        print('')
+
+        while True:
+            continue_ = get_input('Do you want to continue? [Y/n]\n', default='y').lower()
+
+            match continue_:
+                case 'y' | 'yes' | 'ok':
+                    break
+                case 'n' | 'no' | 'nope':
+                    exit(0)
+                case _:
+                    continue
+
+    except InvalidUrl as err:
+        print(err)
+        continue
+
+    except Exception as err:
+        print('\nSomething went wrong!')
+        print(f'URL: {album_url}')
+        print(f'Error: {err}')
+        print('\nKindly bring this error to the attention of the seller by reporting it to them. Thank you.')
+
+    finally:
+        print('')
